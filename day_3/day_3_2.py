21892b494a775f25aa4b118990b3b3a4a5dd259e
Ohmaha40 <keller.chris@outlook.de>
2022-12-03 14:24:11 +0100

    day 3 part 2

--- day_3/day_3_2.py
+++ day_3/day_3_2.py
@@ -5,29 +5,28 @@ from time import perf_counter as pfc
 def read_task(file):
     with open(file) as f:
         listeg = []
+        group= []
+        count = 0
         for row in f:
-            liste = []
-            liste.append(row[:-(int(len(row)/2+1))])
-            liste.append(row[(int(len(row)/2)):].strip("\n"))
-            listeg.append(liste)
+            if count < 3:
+                count += 1
+                group.append(row.strip("\n"))
+                if count == 3:
+                    listeg.append(group)
+                    group = []
+                    count = 0
+
         return listeg
 def solve(task):
     punkte = 0
     werte = list(string.ascii_letters[:52])
     for liste in task:
         for i in liste[0]:
-            if i in liste[1]:
-                punkte += werte.index(i) +1
+            if i in liste[1] and i in liste[2]:
+                punkte += werte.index(i)+1
                 break
     return punkte
 
 start=pfc()
-print(solve(read_task('day_3.txt')))
+print(solve(read_task("day_3.txt")))
 print(pfc()-start)
-
-#werte = list(string.ascii_letters[:52])
-#werte.append(list(string.ascii_uppercase[:25]))
-#print(werte[51])
-#print(werte[int(len(werte)/2)])
-#print(werte.index("A"))
-#read_task("day_3.txt")
